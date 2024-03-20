f0249ace9e18fff79f4a1e26a6ec6176da0ed2e7
Gabriel Diniz <gabriel.diniz@inda.band>
2024-03-20 11:51:33 -0300

    mend

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
-	"time"
 
 	"github.com/dinizgab/buildco-api/config"
 	"github.com/go-chi/chi/v5"
@@ -17,11 +16,11 @@ func main() {
 	server := http.Server{
 		Addr:         fmt.Sprintf(":%d", config.Server.Port),
 		Handler:      router,
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  config.Server.TimeoutIdle,
+		ReadTimeout:  config.Server.TimeoutRead,
+		WriteTimeout: config.Server.TimeoutWrite,
 	}
 
 	err := server.ListenAndServe()
-	app.Logger.Error("error while setting up server", slog.Any("error", err))
+	appLogger.Error("error while setting up server", slog.Any("error", err))
 }
