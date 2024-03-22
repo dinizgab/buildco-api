ec3b562b0c354f4dc4e5ab6ebccfd27217949724
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-03-22 16:13:11 -0300

    feat: implementing create company handler

--- internal/company/handler/handler.go
+++ internal/company/handler/handler.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log/slog"
 	"net/http"
 
+	"github.com/dinizgab/buildco-api/internal/company/entity"
 	"github.com/dinizgab/buildco-api/internal/company/repository"
 	"github.com/dinizgab/buildco-api/internal/company/usecase"
 )
@@ -24,5 +26,23 @@ func New(logger *slog.Logger, db *sql.DB) *API {
 }
 
 func (api *API) Create(w http.ResponseWriter, r *http.Request) {
+    var company *entity.Company
 
+    err := json.NewDecoder(r.Body).Decode(&company)
+    if err != nil {
+        api.logger.Error("error: ", slog.Any("error", err))
+        return
+    }
+
+    createdCompany, err := api.usecase.Create(company)
+    if err != nil {
+        api.logger.Error("error: ", slog.Any("error", err))
+        return
+    }
+
+    err = json.NewEncoder(w).Encode(createdCompany)
+    if err != nil {
+        api.logger.Error("error: ", slog.Any("error", err))
+        return
+    }
 }
