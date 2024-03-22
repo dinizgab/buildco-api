1cc4ce6341bce4afaece35249692121a24787fa3
Gabriel Diniz <gabriel.diniz@inda.band>
2024-03-22 10:11:57 -0300

    feat: creating company table migration

--- migrations/20240322125245_create_company_table.sql
+++ migrations/20240322125245_create_company_table.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS company(
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+CREATE TABLE IF NOT EXISTS company (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
     phone TEXT NOT NULL,
     email TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT NOW()
 );
 -- +goose StatementEnd
 
